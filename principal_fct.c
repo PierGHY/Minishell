bd0e996254d5c842720a2aece317a22d1ef46c4e
GHYZEl Pierre <gpierre@localhost.localdomain>
2019-03-14 15:21:53 +0100

    zz

--- principal_fct.c
+++ principal_fct.c
@@ -72,7 +72,7 @@ void fct_fork(char *go, char ** str, char **envp, int status)
     if (id == 0 ) {
         if (execve(str[0], str, envp) == -1) {
             my_pute(str[0]);
-            write(2, ": Command not found.\n", 22);
+            write(2, ":Command not found.\n", 22);
             exit (1);
         }
         exit(0);
